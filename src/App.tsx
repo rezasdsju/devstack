bac9a7a5f7a19bc575afd382716446f6d61cfa22
rezasdsju <rezaul2002104@gmail.com>
2026-09-10 21:58:12 +0600

    feat: add Nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Nav from "./components/Nav"
 
 function App() {
 
 
   return (
     <>
-
+      <Nav></Nav>
     </>
   )
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,24 @@
+
+import { GiHamburgerMenu } from 'react-icons/gi'
+import NavLogo from '../assets/logo-text.png'
+export default function Nav() {
+  return (
+    <nav className='sticky flex md:grid md:grid-cols-3 pt-3 pb-3 pl-2 shadow-md'>
+        <img src={NavLogo} alt="" />
+        <ul className='hidden md:flex gap-5 md:items-center'>
+            <li>Home</li>
+            <li>Technologies</li>
+            <li>Projects</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        <div className='flex items-center mx-auto gap-2 md:gap-6'>
+            <button className="btn btn-soft">Sign In</button>
+            <button className="btn btn-secondary">Sign Up</button>
+        </div>
+        <span className='flex items-center md:hidden'><GiHamburgerMenu /></span>
+
+
+    </nav>
+  )
+}

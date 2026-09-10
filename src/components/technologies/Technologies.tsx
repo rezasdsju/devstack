021e585173c7a6e52d1edae012f54ed9330a9a7f
rezasdsju <rezaul2002104@gmail.com>
2026-09-10 23:33:37 +0600

    style: improve technology card design

--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -14,7 +14,7 @@ export default function Technologies({ technologiesPromise }: TechnologiesPropsT
             <h2 className="text-3xl font-bold">Explore the <span className="text-[#D946EF]">Technologies</span></h2>
             <p>Pick one technology per category to build your ideal stack.</p>
             <div className="grid grid-cols-1 sm:grid-cols-12">
-                <div className="sm:col-span-9 grid grid-cols-1 sm:grid-cols-3 sm: gap-5">
+                <div className="sm:col-span-9 grid grid-cols-1 sm:grid-cols-3 sm: gap-5 ">
                     
                         {
                             technologies.map(technology => <TechnologiesCard
--- src/components/technologies/technologiesCard/TechnologiesCard.tsx
+++ src/components/technologies/technologiesCard/TechnologiesCard.tsx
@@ -5,21 +5,35 @@ interface TechnologyPropsTypes {
     technology: TechnologiesType
 }
 export default function TechnologiesCard({ technology }: TechnologyPropsTypes) {
+    const badgeColors: Record<string, string> = {
+        Popular: "bg-blue-100 text-blue-700",
+        Trending: "bg-blue-100 text-blue-700",
+        React: "bg-blue-100 text-blue-700",
+
+        "Top SQL": "bg-green-100 text-green-700",
+        NoSQL: "bg-green-100 text-green-700",
+
+        Essential: "bg-purple-100 text-purple-700",
+        Python: "bg-purple-100 text-purple-700",
+
+        Backend: "bg-orange-100 text-orange-700",
+        DevOps: "bg-orange-100 text-orange-700",
+    }
     return (
-        <div className="border border-gray-100 sm: gap-5">
-            <div className="flex justify-between">
+        <div className="border border-gray-100 rounded-2xl pt-3 pb-3 space-y-2">
+            <div className="flex justify-between mx-2">
                 <img src={technology.icon} alt="" className="h-10" />
-                <p>{technology.badge}</p>
+                <button className={`${badgeColors[technology.badge]} rounded-xl px-4 `}>{technology.badge && technology.badge}</button>
             </div>
-            <h2 className="font-bold text-2xl">{technology.name}</h2>
-            <p>{technology.description}</p>
-            <div className="flex gap-5">
+            <h2 className="font-bold text-2xl pl-2">{technology.name}</h2>
+            <p className="pl-2">{technology.description}</p>
+            <div className="flex gap-5 items-center border-t border-slate-200 w-[90%] mx-4 pt-2 pb-2">
                 <button className="btn btn-soft">{technology.category}</button>
                 <p>{technology.difficulty}</p>
-                <p><FaStar />{technology.rating}</p>
+                <p className="flex items-center"><FaStar />{technology.rating}</p>
 
             </div>
-            <button className="text-white px-10">Add to Stack</button>
+            <button className="flex items-center bg-black text-white px-14 rounded-2xl mx-auto py-2">Add to Stack</button>
         </div>
     )
 }

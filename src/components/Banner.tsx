27a37109b21718f745c433c055b5f74ecffa5b8b
rezasdsju <rezaul2002104@gmail.com>
2026-09-10 22:21:58 +0600

    feat: add Banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,22 @@
+import BannerImage from '../assets/banner-stack.png'
+export default function Banner() {
+    return (
+        <div className='grid grid-cols-1 sm:grid-cols-2 items-center pl-3 sm:pl-6 pt-3'>
+            <div className='space-y-3 sm:space-y-4'>
+                <h1 className='text-3xl'>Build Your Ideal <br></br> <span className="bg-linear-to-r from-[#ff4b2b] via-[#e91e63] to-[#8e2de2] bg-clip-text text-transparent font-semibold">Development Stack</span></h1>
+                <p>Explore frontend, backend, database, and tooling options, <br></br>
+                    compare them side by side, and put together the stack that fits your
+                    next project.</p>
+<div className='flex items-center gap-3 sm:gap-5'>
+                    <button className="btn btn-active btn-secondary">Explore Technologies</button>
+                <button className="btn btn-soft">Learn More</button>
+</div>
+
+
+            </div>
+            <div>
+                <img src={BannerImage} alt="" className='h-85 mx-auto'/>
+            </div>
+        </div>
+    )
+}
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,7 @@ import { GiHamburgerMenu } from 'react-icons/gi'
 import NavLogo from '../assets/logo-text.png'
 export default function Nav() {
   return (
-    <nav className='sticky flex md:grid md:grid-cols-3 pt-3 pb-3 pl-2 shadow-md'>
+    <nav className='sticky top-0 z-50 bg-white flex md:grid md:grid-cols-3 pt-3 pb-3 pl-2 shadow-md'>
         <img src={NavLogo} alt="" />
         <ul className='hidden md:flex gap-5 md:items-center'>
             <li>Home</li>

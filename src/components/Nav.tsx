ff05fb0dd5cdbc3ee1f472e2363591fc1bbc58d0
rezasdsju <rezaul2002104@gmail.com>
2026-09-11 10:09:20 +0600

    style: move hamburger icon to left

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,6 +4,7 @@ import NavLogo from '../assets/logo-text.png'
 export default function Nav() {
   return (
     <nav className='sticky top-0 z-50 bg-white flex md:grid md:grid-cols-3 pt-3 pb-3 pl-2 shadow-md'>
+        <span className='flex items-center md:hidden'><GiHamburgerMenu /></span>
         <img src={NavLogo} alt="" />
         <ul className='hidden md:flex gap-5 md:items-center'>
             <li>Home</li>
@@ -16,7 +17,7 @@ export default function Nav() {
             <button className="btn btn-soft">Sign In</button>
             <button className="btn btn-secondary">Sign Up</button>
         </div>
-        <span className='flex items-center md:hidden'><GiHamburgerMenu /></span>
+        {/* <span className='flex items-center md:hidden'><GiHamburgerMenu /></span> */}
 
 
     </nav>
